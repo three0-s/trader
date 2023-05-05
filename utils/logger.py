cc7aeb35c4579747884bd15da4f2fdfab1014053
Yewon Lim <ga06033@yonsei.ac.kr>
2023-05-05 17:46:04 +0900

    230505 | logger and scheduler

---
+++ utils/logger.py
@@ -0,0 +1,34 @@
+# Code referenced from https://gist.github.com/gyglim/1f8dfb1b5c82627ae3efcfbbadb9f514
+import tensorflow as tf
+import numpy as np
+from torchvision.transforms import ToTensor
+from torch.utils.tensorboard import SummaryWriter
+
+class Logger(object):
+    
+    def __init__(self, log_dir):
+        """Create a summary writer logging to log_dir."""
+        self.writer = SummaryWriter(log_dir)
+
+    def scalar_summary(self, tag, value, step):
+        """Log a scalar variable."""
+        self.writer.add_scalar(tag=tag, scalar_value=value, global_step=step)
+
+    def image_summary(self, tag, images, step):
+        """Log a list of PIL images."""
+
+        for i, img in enumerate(images):
+            tensorImg = ToTensor(img)
+            # Create a Summary value
+            self.writer.add_image('%s/%d' % (tag, i), tensorImg, step)
+
+
+    def histo_summary(self, tag, values, step, bins=1000):
+        """Log a histogram of the tensor of values."""
+
+        # Create a histogram using numpy
+        self.writer.add_histogram(tag=tag, values=values)
+        self.writer.flush()
+
+    def close(self):
+        self.writer.close()
